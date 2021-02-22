1b0b05eabdb1d453353d80ab8a38e4ea41655664
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-22 13:12:00 +0100

    Initial

--- StudentProjectAllocation/My Project/Application.Designer.vb
+++ StudentProjectAllocation/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.StudentProjectAllocation.Form1
+            Me.MainForm = Global.StudentProjectAllocation.frmSplash
         End Sub
     End Class
 End Namespace
---
+++ StudentProjectAllocation/frmSplash.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmSplash
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+        Me.CenterToScreen()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+End Class
