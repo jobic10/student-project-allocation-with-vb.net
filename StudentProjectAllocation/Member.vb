bba8c9d2affc6a8ed5a6941cf2aea769ea1d1e6b
JOB OWONUBI <jobowonubi@gmail.com>
2021-03-03 15:24:05 -0800

    Code Refactor For Class Member

---
+++ StudentProjectAllocation/Member.vb
@@ -0,0 +1,15 @@
+﻿Public Class Member
+    'Start of Properties
+    Public FullName As String
+    Public MatricNumber As String
+    Public Photo As String
+    Public Role As String
+
+    Public Sub New(fullname As String, regno As String, avatar As String, role As String)
+        Me.FullName = fullname
+        Me.MatricNumber = regno
+        Me.Photo = avatar
+        Me.Role = role
+        Console.WriteLine("New Student Created")
+    End Sub
+End Class
